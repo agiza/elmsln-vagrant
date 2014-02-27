7b451f6c52b1daa56c992b95e072359573caf7b0
btopro <bto108@psu.edu>
2014-02-27 10:13:08 -0500

    moving elms stuff to front of list so its faster to see if the provisioning fails

--- roles/elmsln_dev.rb
+++ roles/elmsln_dev.rb
@@ -1,12 +1,12 @@
 name "elmsln_dev"
 description "A fully configured ELMSLN instance for testing and development."
 run_list(
+  "recipe[elmsln]",
+  "recipe[elmsln-config-vagrant]",
   "role[apache2_mod_php]",
   "role[drupal]",
   "role[drupal_dev]",
   "role[memcached]",
   "role[mysql_server]",
-  "recipe[drupal::multisite]",
-  "recipe[elmsln]",
-  "recipe[elmsln-config-vagrant]"
+  "recipe[drupal::multisite]"
 )
