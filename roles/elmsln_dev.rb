b44277b65fee0c6ad772232b2a32f6ce63cd6de8
btopro <bto108@psu.edu>
2014-02-27 11:53:53 -0500

    needs shifted after apache is added, duh

--- roles/elmsln_dev.rb
+++ roles/elmsln_dev.rb
@@ -1,13 +1,13 @@
 name "elmsln_dev"
 description "A fully configured ELMSLN instance for testing and development."
 run_list(
-  "recipe[elmsln::default]",
-  "recipe[elmsln-config-vagrant]",
   "role[apache2_mod_php]",
   "role[drupal]",
   "role[drupal_dev]",
   "role[memcached]",
   "role[mysql_server]",
   "recipe[drupal::multisite]",
+  "recipe[elmsln::default]",
+  "recipe[elmsln-config-vagrant]",
   "recipe[elmsln::install]"
 )
