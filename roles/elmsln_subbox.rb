7bf92c9767c673342104c77088079da149723d13
btopro <bto108@psu.edu>
2014-03-13 11:56:35 -0400

    sub box routine updated now that less happens in the VM Out of the box, the sub-box will run through the elmsln installer

--- roles/elmsln_subbox.rb
+++ roles/elmsln_subbox.rb
@@ -1,6 +1,10 @@
 name "elmsln_subbox"
-description "Do git checkouts for latest versions of code, this is for use with the elms-vagrant.box prebuilt environment."
+description "Do git checkouts for latest versions of code, this is for use with the elms-vagrant.box prebuilt base environment. Then run the installer."
 run_list(
-  "recipe[elmsln::default]",
-  "recipe[elmsln-config-vagrant::default]"
+  "recipe[elmsln::repo]",
+  "recipe[elmsln-config-vagrant::default]",
+  "recipe[elmsln-config-vagrant::drush-aliases]",
+  "recipe[elmsln-config-vagrant::piwik-directories]",
+  "recipe[elmsln::install]",
+  "recipe[elmsln-config-vagrant::vagrant-install]"
 )
